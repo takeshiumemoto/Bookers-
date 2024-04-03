c78b99337a8bbddc37f3232e79fe1546a17524fe
EC2 Default User <ec2-user@ip-172-31-23-212.ap-northeast-1.compute.internal>
2024-04-03 16:48:59 +0000

    Initial Commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,52 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.order(:id)
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if  @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render :index,status: :unprocessable_entity
+    end 
+  end  
+
+  def update
+    @book= Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book)
+    else
+      render :edit,status: :unprocessable_entity
+    end
+  end
+  
+  def edit
+   @book = Book.find(params[:id])
+  end
+  
+  
+  def show
+    @book =Book.find(params[:id])
+  end
+
+  def destroy
+    @book= Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end 
+  
+  private
+  def book_params
+  params.require(:book).permit(:title, :body)
+  end 
+end 
